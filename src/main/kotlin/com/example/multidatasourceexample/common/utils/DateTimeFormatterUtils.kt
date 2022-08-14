db14c0d72269c91243aba54772896293bee0a2c8
janghyoseog <bestdevhyo1225@gmail.com>
2022-08-14 19:49:35 +0900

    feat: DateTimeFormatterUtils 클래스 추가

---
+++ src/main/kotlin/com/example/multidatasourceexample/common/utils/DateTimeFormatterUtils.kt
@@ -0,0 +1,11 @@
+package com.example.multidatasourceexample.common.utils
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+object DateTimeFormatterUtils {
+    private val FORMATTER_PATTERN = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
+    fun toStringDateTime(localDateTime: LocalDateTime): String = localDateTime.format(FORMATTER_PATTERN)
+    fun toLocalDateTime(stringDateTime: String): LocalDateTime = LocalDateTime.parse(stringDateTime, FORMATTER_PATTERN)
+}
