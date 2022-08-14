6d11c2b431ab4c8449f8e3e5aa15a3c5bc86f57d
janghyoseog <bestdevhyo1225@gmail.com>
2022-08-14 20:55:09 +0900

    feat: Pay 관련 예외 메시지 추가

--- src/main/kotlin/com/example/multidatasourceexample/common/constants/ExceptionMessage.kt
+++ src/main/kotlin/com/example/multidatasourceexample/common/constants/ExceptionMessage.kt
@@ -6,4 +6,8 @@ object ExceptionMessage {
     const val ORDER_STATUS_NOT_EXISTS = "존재하지 않는 주문 상태입니다."
     const val ORDER_ITEM_CATEGORY_NOT_EXISTS = "존재하지 않는 주문 아이템 카테고리입니다."
     const val ORDER_NOT_FOUND = "해당 주문이 존재하지 않습니다."
+
+    const val PAY_STATUS_IS_CANCEL = "결제 취소 상태에서는 다른 상태로 변경할 수 없습니다."
+    const val PAY_STATUS_IS_FAIL = "결제 실패 상태에서는 다른 상태로 변경할 수 없습니다."
+    const val PAY_STATUS_NOT_EXISTS = "존재하지 않는 결제 상태입니다."
 }
