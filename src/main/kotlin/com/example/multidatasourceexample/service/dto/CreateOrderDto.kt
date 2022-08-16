255b33cd2b4c5057bbcb382807ab7ca3c5689612
janghyoseog <bestdevhyo1225@gmail.com>
2022-08-17 00:40:34 +0900

    refactor: 생성 결과 Dto도 한 파일에서 관리하도록 수정

--- src/main/kotlin/com/example/multidatasourceexample/service/dto/CreateOrderDto.kt
+++ src/main/kotlin/com/example/multidatasourceexample/service/dto/CreateOrderDto.kt
@@ -1,5 +1,7 @@
 package com.example.multidatasourceexample.service.dto
 
+import com.example.multidatasourceexample.domain.order.entity.Order
+
 data class CreateOrderDto(
     val memberId: Long,
     val createOrderItemsDto: CreateOrderItemsDto,
@@ -14,3 +16,11 @@ data class CreateOrderItemDto(
     val itemName: String,
     val purchasePrice: Float,
 )
+
+data class CreateOrderResultDto(
+    val orderId: Long,
+) {
+    companion object {
+        fun of(order: Order) = CreateOrderResultDto(orderId = order.id)
+    }
+}
